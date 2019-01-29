147127aa800ae77c1335960ac9d3b6dcf53db9b9
Jonas Huber <>
2019-01-29 23:42:17 +0100

    ESLint & Prettier adjustments

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,13 +1,13 @@
-import * as functions from "firebase-functions"
-import next from "next"
+import * as functions from 'firebase-functions'
+import next from 'next'
 
-const dev = process.env.NODE_ENV !== "production"
-const app = next({ dev, conf: { distDir: "next" } })
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, conf: { distDir: 'next' } })
 const handle = app.getRequestHandler()
 
 export const nextApp = functions.https.onRequest((request, response) => {
-  console.log("File: " + request.originalUrl)
-  console.log("dev:", dev)
+  // console.log('File: ' + request.originalUrl)
+  // console.log('dev:', dev)
   // log the page.js file or resource being requested
 
   return app.prepare().then(() => handle(request, response))
