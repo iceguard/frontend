ce89282822000d812f4a669570df52e32d48ae43
Jonas Huber <>
2019-02-27 00:24:08 +0100

    Function test

--- nextAppFunction/index.js
+++ nextAppFunction/index.js
@@ -4,23 +4,29 @@
 // const app = next({ dev, conf: { distDir: 'next' } })
 // const handle = app.getRequestHandler()
 
-module.exports = function() {
-    return 'hello world!'
-    // return app.prepare().then(function() {
-    //     return handle(req, context.res)
-    // })
-
-    // context.log('JavaScript HTTP trigger function processed a request.')
-
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-    //         // status: 200, /* Defaults to 200 */
-    //         body: 'Hello !! ' + (req.query.name || req.body.name),
-    //     }
-    // } else {
-    //     context.res = {
-    //         status: 400,
-    //         body: 'Please pass a name on the query string or in the request body',
-    //     }
-    // }
+module.exports = async function() {
+    return {
+        body: 'Ice Guard Frontend',
+    }
 }
+
+// module.exports = function() {
+//     return 'hello world!'
+// return app.prepare().then(function() {
+//     return handle(req, context.res)
+// })
+
+// context.log('JavaScript HTTP trigger function processed a request.')
+
+// if (req.query.name || (req.body && req.body.name)) {
+//     context.res = {
+//         // status: 200, /* Defaults to 200 */
+//         body: 'Hello !! ' + (req.query.name || req.body.name),
+//     }
+// } else {
+//     context.res = {
+//         status: 400,
+//         body: 'Please pass a name on the query string or in the request body',
+//     }
+// }
+// }
