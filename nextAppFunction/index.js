d2dce371539df5474999c6a26217d2c16b4fadf4
Jonas Huber <>
2019-02-27 00:28:39 +0100

    Function test with Next.js

--- nextAppFunction/index.js
+++ nextAppFunction/index.js
@@ -1,12 +1,14 @@
-// const next = require('next')
+const next = require('next')
 
-// const dev = process.env.NODE_ENV !== 'production'
-// const app = next({ dev, conf: { distDir: 'next' } })
-// const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, conf: { distDir: 'next' } })
+const handle = app.getRequestHandler()
 
-module.exports = async function() {
+module.exports = async function(context, req) {
     return {
-        body: 'Ice Guard Frontend',
+        body: app.prepare().then(function() {
+            return handle(req, context.res)
+        }),
     }
 }
 
