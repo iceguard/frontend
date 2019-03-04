29a63bb693d0523dc1a0d59232121c5afd9da814
Jonas Huber <>
2019-03-04 20:28:15 +0100

    Remove styles

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import Main from '@layout/Main'
-import style from './index.css'
 
 export default class IndexPage extends Component {
     render() {
         return (
             <Main>
-                <h2 className={style.test}>Welcome to Ice Guard!</h2>
+                <h2>Welcome to Ice Guard!</h2>
             </Main>
         )
     }
