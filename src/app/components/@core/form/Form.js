4c15f45ff6984c12af86b33019a8c1bb4544902f
Jonas Huber <>
2019-03-11 20:29:48 +0100

    Style forms

--- src/app/components/@core/form/Form.js
+++ src/app/components/@core/form/Form.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import styles from './form.scss'
 
 export default class Form extends Component {
     render() {
         const { onSubmit, children } = this.props
 
-        return <form onSubmit={onSubmit}>{children}</form>
+        return (
+            <form onSubmit={onSubmit} className={styles.FormContainer}>
+                {children}
+            </form>
+        )
     }
 }
--- src/app/components/@core/form/FormInput.js
+++ src/app/components/@core/form/FormInput.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
+import styles from './form.scss'
 
 export default class FormInput extends Component {
     render() {
         const { label, name, type, required, initialValue, errorMessage, onChange, hasError } = this.props
 
         return (
-            <div>
-                <label htmlFor={name}>{label}</label>
-                <input id={name} type={type} name={name} required={required} value={initialValue} autoComplete="off" onChange={onChange} />
-                {hasError && <div>{errorMessage}</div>}
+            <div className={classnames(styles.formItem, { [styles.formItemError]: hasError })}>
+                <label htmlFor={name} className={styles.formLabel}>
+                    {label}
+                </label>
+                <input
+                    id={name}
+                    className={styles.formInput}
+                    type={type}
+                    name={name}
+                    required={required}
+                    value={initialValue}
+                    autoComplete="off"
+                    onChange={onChange}
+                />
+                {hasError && <div className={styles.formError}>{errorMessage}</div>}
             </div>
         )
     }
