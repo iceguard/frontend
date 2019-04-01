b203f2bc544c90787e87acc074f909a65bfa759a
Jonas Huber <>
2019-04-01 19:29:17 +0200

    Add dummy devices

--- src/app/components/@section/deviceOverview/DeviceOverview.js
+++ src/app/components/@section/deviceOverview/DeviceOverview.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
 import styles from './deviceOverview.css'
+import { Icon } from '@misc/icon'
+import Link from 'next/link'
 
 export class DeviceOverview extends Component {
     constructor() {
@@ -11,8 +13,8 @@ export class DeviceOverview extends Component {
         }
     }
 
-    componentDidMount() {
-        fetch('https://iceguard-cosmos-functions.azurewebsites.net/api/measurements?deviceId=simulator')
+    fetchData() {
+        fetch('https://iceguard-cosmos-functions.azurewebsites.net/api/latest?deviceIds=simulator,simulator2')
             .then(response => {
                 return response.json()
             })
@@ -23,30 +25,40 @@ export class DeviceOverview extends Component {
             })
     }
 
+    componentDidMount() {
+        this.fetchData()
+
+        setInterval(() => {
+            this.fetchData()
+        }, 2000)
+    }
+
     render() {
         return (
             <section className={styles.deviceOverview}>
-                {/* {this.state.data.map((device, index) => {
+                {this.state.deviceData.map((device, index) => {
                     return Device(device, index)
-                })} */}
-                {Device(this.state.deviceData[0], 0)}
+                })}
             </section>
         )
     }
 }
 
 const Device = (device, index) => {
-    if (!device) {
-        return null
-    }
-
     return (
-        <div className={classnames(styles.device)} key={`device-${index}`}>
+        <div className={classnames(styles.device, { [styles.alert]: device.alert })} key={`${device.deviceId}-${index}`}>
             <div className={styles.deviceHead}>
                 <div className={styles.status}>
                     <div className={styles.statusDot} />
                 </div>
                 <div className={styles.title}>{device.deviceId}</div>
+                <div className={styles.showDetailLink}>
+                    <Link href="/device">
+                        <a>
+                            <Icon type="settings" size="25" />
+                        </a>
+                    </Link>
+                </div>
             </div>
             <div className={styles.deviceContent}>
                 <div className={styles.deviceTile}>
@@ -58,29 +70,12 @@ const Device = (device, index) => {
                     <div className={styles.tileInfo}>{parseFloat(device.temperature).toFixed(2)} °C</div>
                 </div>
             </div>
+            <div className={styles.deviceFoot}>
+                <div className={styles.deviceTile}>
+                    <h5 className={styles.tileHeader}>Last Update</h5>
+                    <div className={styles.tileInfo}>{device.timestamp}</div>
+                </div>
+            </div>
         </div>
     )
 }
-
-// const Device = (device, index) => {
-//     return (
-//         <div className={classnames(styles.device, { [styles.alert]: device.alert })} key={`${device.name}-${index}`}>
-//             <div className={styles.deviceHead}>
-//                 <div className={styles.status}>
-//                     <div className={styles.statusDot} />
-//                 </div>
-//                 <div className={styles.title}>{device.name}</div>
-//             </div>
-//             <div className={styles.deviceContent}>
-//                 {device.tiles.map((tile, index) => {
-//                     return (
-//                         <div className={styles.deviceTile} key={`${tile.name}-${index}`}>
-//                             <h5 className={styles.tileHeader}>{tile.name}</h5>
-//                             <div className={styles.tileInfo}>{tile.value}</div>
-//                         </div>
-//                     )
-//                 })}
-//             </div>
-//         </div>
-//     )
-// }
