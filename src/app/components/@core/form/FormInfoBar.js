fba90f9187819335d001966b91b5d5a2ae812374
Jonas Huber <>
2019-04-26 12:39:39 +0200

    Form info message styles

--- src/app/components/@core/form/FormInfoBar.js
+++ src/app/components/@core/form/FormInfoBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import style from './form.css'
 
 export default class FormInfoBar extends Component {
     render() {
         const { infoMessage } = this.props
 
-        return <div>{infoMessage}</div>
+        return <div className={style.formInfo}>{infoMessage}</div>
     }
 }
